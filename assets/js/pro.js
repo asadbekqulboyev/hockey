1afaf005d072ffcb79db33e8436653b358576223
asadbek <asadbekqulboyev@gmail.com>
2025-03-07 10:09:13 +0500

    finish

--- assets/js/pro.js
+++ assets/js/pro.js
@@ -24,12 +24,43 @@
                 prevEl: ".offer_slide .prev",
             },
         });
-        const swiper2 = new Swiper(".participants_slide", {
+        const swiper2 = new Swiper(".participants_slides", {
             spaceBetween:20,
+            slidesPerView:4.5,
             navigation: {
                 nextEl: ".participants .next",
                 prevEl: ".participants .prev",
             },
+            breakpoints: {
+                0:{
+                    slidesPerView: 1,
+                },
+                640: {
+                  slidesPerView: 2, // 640px va undan katta ekranda 2 ta slayd ko'rinadi
+                },
+                768: {
+                  slidesPerView: 3, // 768px va undan katta ekranda 3 ta slayd
+                },
+                1024: {
+                  slidesPerView: 4.5, // 1024px va undan katta ekranda 4 ta slayd
+                }
+            }
+        });
+        // faq
+        $(".faq_header").click(function(){
+            var content = $(this).next(".faq_description");
+            // Barcha ochilganlarni yopish
+            $(".faq_description").not(content).slideUp();
+            $(".accordion_icon").text("+");
+            
+            // Bosilgan elementni ochish yoki yopish
+            if(content.is(":visible")){
+                content.slideUp();
+                $(this).find(".accordion_icon").text("+");
+            } else {
+                content.slideDown();
+                $(this).find(".accordion_icon").text("-");
+            }
         });
     });  
 
